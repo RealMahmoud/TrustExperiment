b8d886b9e55fde6b90e1e8bd4c4cb02d873d4f37
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-08-22 17:28:45 +0200

    FIXES

--- js/preload.js
+++ js/preload.js
@@ -7,11 +7,8 @@ var mode = localStorage.getItem('mode') || '';
 if (mode == 'light') {
   document.getElementsByTagName('html')[0].classList.remove('darkmode');
   document.querySelector('.header_logo a img').setAttribute('src', './images/logo_light.png');
-  document.getElementById('moon').classList.remove('rem');
-  document.getElementById('sun').classList.add('rem');
 } else {
   document.getElementsByTagName('html')[0].classList.add('darkmode');
   document.querySelector('.header_logo a img').setAttribute('src', './images/logo_black.png');
-  document.getElementById('moon').classList.add('rem');
-  document.getElementById('sun').classList.remove('rem');
+
 }
